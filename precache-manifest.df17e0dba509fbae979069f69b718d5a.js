67b05f6c8fc2b79e20cdadd860f18eb865c2e14b
Aditya Varshney <adityavarshney@gmail.com>
2020-04-25 23:05:44 -0700

    Updates

---
+++ precache-manifest.df17e0dba509fbae979069f69b718d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae175c39d14f4abf5c85b136d6fc3ec",
+    "url": "/app-reader-test-deploy/index.html"
+  },
+  {
+    "revision": "6ab56216c915687fcdcf",
+    "url": "/app-reader-test-deploy/static/css/main.e555a225.chunk.css"
+  },
+  {
+    "revision": "bb339ae9efc11cce56c2",
+    "url": "/app-reader-test-deploy/static/js/2.698c19d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-reader-test-deploy/static/js/2.698c19d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab56216c915687fcdcf",
+    "url": "/app-reader-test-deploy/static/js/main.d101a07e.chunk.js"
+  },
+  {
+    "revision": "3f3addf9f44a754db066",
+    "url": "/app-reader-test-deploy/static/js/runtime-main.a89e4974.js"
+  }
+]);
