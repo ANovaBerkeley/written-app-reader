a35e7816df72252ad39db1cbf6840587b18100aa
Aditya Varshney <adityavarshney@gmail.com>
2020-05-01 23:25:36 -0700

    airtable regen, cleanup

--- src/global.js
+++ src/global.js
@@ -2,9 +2,12 @@
 // The global scope in React Native is variable global.
 // For ex: as global.foo = foo, then you can use global.foo anywhere as a global variable.
 // Make sure you import './global.js' in your files to use global variables!
-global.AIRTABLE_KEY = "keyd6qkfF0Q6csT9G"; 
-global.APPLICATIONS_URL = "https://api.airtable.com/v0/appm1EwjHL56mOmPx/All%20Applications"; // Applications airtable link
-global.DECISIONS_URL = "https://api.airtable.com/v0/appm1EwjHL56mOmPx/Decisions"; // Decisions airtable link
+
+global.AIRTABLE_KEY = "keyZ8W3ko0tHQO5zV"; // keep this secret
+
+/** Change these every semester */
+global.BASE_ID = "appm1EwjHL56mOmPx";
+global.SEM_SECRET = "993342";
 global.OFFICERS = [
     "Aditya Varshney",
     "Anna Gao",
@@ -12,7 +15,11 @@ global.OFFICERS = [
     "Hau Nguyen",
     "Andrew Lieu"
 ];
-global.SEM_SECRET = "993342"
+
+global.APPLICATIONS_URL = "https://api.airtable.com/v0/"+global.BASE_ID+"/All%20Applications"; // Applications airtable link
+global.DECISIONS_URL = "https://api.airtable.com/v0/"+global.BASE_ID+"/Decisions"; // Decisions airtable link
+
+/** Specific to the process every semester */
 global.NUM_YES = 30;
 global.IGNORED_FIELDS = [
     "Name",
@@ -20,10 +27,11 @@ global.IGNORED_FIELDS = [
     "Year",
     "Phone Number",
 ];
-/** Custom ordering of questions */
 global.QUESTION_ORDER = [
-    6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,0,1,2,3,4,5,
+    6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,0,1,2,3,4,5, // based on ordering of airtable fields (optional)
 ]
+
+/** Guidelines page markdown content (change as needed) */
 global.INSTRUCTIONS = `
 # Recruitment guidelines
 Please review this page for instructions, reminders, and tips on how to proceed app-reading. 
